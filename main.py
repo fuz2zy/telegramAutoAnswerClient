33b5f5c9f2b167b6d8f7976a3edbeb95bcbf0a51
fuz2zy <fuzzy.garp@gmail.com>
2026-03-14 17:06:17 +0300

    .

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ async def on_private_message(event: events.NewMessage.Event):
         return
     
     sender_id = event.sender_id
-
+    
     last_answer_time = last_answer.get(sender_id, 0)
 
     if not time.time() - last_answer_time > 600:
@@ -43,8 +43,6 @@ async def on_private_message(event: events.NewMessage.Event):
                       
 <b>Царь Батюшка был оповещен о вашем обращении и вскоре ответит ✍🏻</b>
 """, parse_mode="html")
-
-    await client.send_file(event.sender_id, file="AAMCAgADGQEC7No4abSPvsb7TjdcgLckN0EC_D7JMzcAAoWeAAKQ4alJLItjNhFv2EQBAAdtAAM6BA")
 
 
 async def main():
